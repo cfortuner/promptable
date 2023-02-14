c584714870634eae51858c98d261b95f510a22a2
Colin Fortuner <cfortuner@gmail.com>
2023-02-14 09:04:18 -0500

    rm unused code from docs

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -1,9 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Promptable.js",
@@ -14,19 +11,10 @@ const config = {
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "promptable", // Usually your GitHub org/user name.
-  projectName: "promptable", // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
   },
-  // clientModules: [require.resolve("./docusaurus.preferredTheme.js")],
 
   presets: [
     [
@@ -35,35 +23,10 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl: "https://github.com/bridge-codes/bridge/tree/main/www",
-        // },
-        // theme: {
-        // customCss: require.resolve("./src/css/custom.css"),
-        // },
       },
     ],
-    // [
-    //   "docusaurus-preset-shiki-twoslash",
-    //   {
-    //     // Not sure how reliable this path is (it's relative from the preset package)?
-    //     // None of the light themes had good support for `diff` mode, so had to patch my own theme
-    //     themes: ["nord"],
-    //   },
-    // ],
   ],
   plugins: [
-    // [
-    // "@docusaurus/plugin-google-analytics",
-    // {
-    //   trackingID: "UA-141789564-1",
-    //   anonymizeIP: true,
-    // },
-    // ],
-
     async function myPlugin() {
       return {
         name: "docusaurus-tailwindcss",
@@ -81,11 +44,6 @@ const config = {
     },
   ],
   themeConfig: {
-    // prism: {
-    // theme: require("prism-react-renderer/themes/nightOwl"),
-    // },
-    // disableSwitch: true,
-    // respectPrefersColorScheme: false,
     navbar: {
       title: "Promptable",
       logo: {
@@ -143,11 +101,6 @@ const config = {
         {
           title: "More",
           items: [
-            // {
-            //   label: "Blog",
-            //   to: "/blog",
-            //   className: "flex items-center",
-            // },
             {
               label: "GitHub",
               href: "https://github.com/cfortuner/promptable",
