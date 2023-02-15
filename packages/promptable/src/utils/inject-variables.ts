1448f5a9b0efdc4bae01770cfb0c82835f8b2595
Colin Fortuner <cfortuner@gmail.com>
2023-02-15 15:18:37 -0500

    adding chat bot example to promptable

--- packages/promptable/src/utils/inject-variables.ts
+++ packages/promptable/src/utils/inject-variables.ts
@@ -1,6 +1,10 @@
-import Handlebars from "handlebars";
-
-export function injectVariables(text: string, variables: any): string {
-  const template = Handlebars.compile(text);
-  return template(variables);
+export function injectVariables(
+  template: string,
+  variables: { [key: string]: any }
+): string {
+  let result = template;
+  for (const key in variables) {
+    result = result.replaceAll(`{{${key}}}`, variables[key]);
+  }
+  return result;
 }
